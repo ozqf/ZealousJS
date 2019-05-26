fb63bd7de163a948ea429a10b2cbdf142b4f1a33
zqf <zqf@zealousquakefan.com>
2019-05-26 21:47:54 +0100

    Basics of refactor of demo into isolated module

--- src/demos/bvh_test.js
+++ src/demos/bvh_test.js
@@ -1,9 +1,28 @@
 
+/*
+
+*/
+
 RegisterDemo("BVH Demo", BVHDemo);
 
 function BVHDemo(rootDiv) {
 
     // Init stuff
+    console.log("Start BVH Demo");
+
+    rootDiv.innerHTML =
+    `<canvas id="canvas"></canvas>`;
+    let canvas = document.getElementById("canvas");
+
+    canvas.setAttribute("class", "world-canvas");
+    let ctx = canvas.getContext("2d");
+    let w = canvas.clientWidth;
+    let h = canvas.clientHeight;
+    ctx.fillStyle = '#000000';
+    ctx.fillRect(0, 0, w, h);
+    console.log(`Canvas: ${w}, ${h}`);
+
+    
 
     this.Destroy = () => {
         // Full shutdown and cleanup

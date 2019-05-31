c26ac5de0d2dee4c3cda7082b1c2d4d8ab4e5f89
zqf <zqf@zealousquakefan.com>
2019-05-31 23:57:49 +0100

    Adding some new files

---
+++ src/demos/super_box_crate_demo.js
@@ -0,0 +1,12 @@
+/*
+Quick JS implementation of Super Crate Box by Vlambeer
+*/
+RegisterDemo("SuperBoxCrate", SuperBoxCrate);
+
+function SuperBoxCrate(rootDiv) {
+	console.log(`Being Super Box Crate`);
+	
+	this.Destroy = () => {
+		
+	};
+}
